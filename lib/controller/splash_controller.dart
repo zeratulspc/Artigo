e71a3baf4ee463539d6260359e3d9f43b3402cb2
zeratulspc <zeratulspc@gmail.com>
2021-11-01 20:08:32 +0900

    로그인 성공 시 Home 으로 이동

--- lib/controller/splash_controller.dart
+++ lib/controller/splash_controller.dart
@@ -1,10 +1,14 @@
 import 'package:artigo2/data/provider/auth_api.dart';
+import 'package:artigo2/ui/home.dart';
 import 'package:get/get.dart';
 
 class SplashController extends GetxController {
   final AuthApi authApi = AuthApi();
 
-  login() {
-    authApi.signInWithGoogle();
+  login() async {
+    var credential = await authApi.signInWithGoogle();
+    if(credential.credential!=null) {
+      Get.offAll(()=>Home());
+    }
   }
 }
